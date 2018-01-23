f5a258e6656e2bf5fea8a8ec929f7433a6616473
JesusFrelseren <HailTheUser@gmail.com>
2018-01-23 14:52:21 +0100

    fragmenter start

--- app/src/main/java/no/erlendhall/erlend/brdskalaen/MainActivity.java
+++ app/src/main/java/no/erlendhall/erlend/brdskalaen/MainActivity.java
@@ -1,10 +1,14 @@
 package no.erlendhall.erlend.brdskalaen;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private Button btnTest;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+    private BottomNavigationView navigator;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_siktet:
                     mTextMessage.setText(R.string.title_home);
+                    fragment = new SiktetFragment();
+		    replaceFragment();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -28,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
-            }
+	    }
             return false;
         }
     };
@@ -38,13 +47,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+	fragmentManager = getSupportFragmentManager();
+
+
+        navigator = (BottomNavigationView) findViewById(R.id.navigation);
+	navigator.inflateMenu(R.menu.bottom_menu);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         btnTest = (Button)findViewById(R.id.button);
-        btnTest.setText("Test");
+        btnTest.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                btnTest.setText("Test");
+            }
+        });
+    }
+
+    private boolean replaceFragment() {
+    	final FragmentTransaction transaction = fragmentManager.beginTransaction();
+	transaction.replace(R.id.main_container, fragment).commit();
+	return true;
+    
     }
 
 }
---
+++ app/src/main/java/no/erlendhall/erlend/brdskalaen/SiktetFragment.java
@@ -0,0 +1,21 @@
+package no.erlendhall.erlend.brdskalaen;
+
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by theuser on 1/23/18.
+ */
+
+public class SiktetFragment extends Fragment{
+
+
+    public SiktetFragment() {
+    }
+        public View onCreateView(LayoutInflater inflater, ViewGroup container) {
+            return inflater.inflate(R.layout.activity_siktet, container, false);
+        }
+
+    }
