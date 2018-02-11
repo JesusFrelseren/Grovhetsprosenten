10402ae4c8f99522074005ecd34895b2c30668df
JesusFrelseren <ErlendAHall@gmail.com>
2018-02-11 18:58:47 +0100

    commit

---
+++ app/src/main/java/no/erlendhall/erlend/brdskalaen/GrovhetKalkulator.java
@@ -0,0 +1,8 @@
+package no.erlendhall.erlend.brdskalaen;
+
+/**
+ * Created by Erlen on 01-Feb-18.
+ */
+
+public class GrovhetKalkulator {
+}
---
+++ app/src/main/java/no/erlendhall/erlend/brdskalaen/TextChangedListener.java
@@ -0,0 +1,8 @@
+package no.erlendhall.erlend.brdskalaen;
+
+/**
+ * Created by Erlen on 04-Feb-18.
+ */
+
+public class TextChangedListener {
+}
